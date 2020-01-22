d3f83511cef876de13873915f58a4de3855caa47
Brandon Green <green.finite@gmail.com>
2020-01-21 19:41:44 -0800

    day-3 setup

---
+++ day-3/taxicab.py
@@ -0,0 +1,12 @@
+# Day 3
+# Crossed Wires
+
+""" Find the intersection point closest to the origin.
+"""
+
+def find_points(ints):
+    """ Given a list of turns denoting a wire's path, return a list of all
+        points in the coordinate plane that the wire crosses
+
+        return type: list of tuples [(x1,y1), (x2,y2), ... ]
+    """
